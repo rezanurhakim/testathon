5980b3ec654737de8028be6f193001ff4979000d
Reza Nurhakim <reza.nurhakim@bukalapak.com>
2018-05-08 00:26:36 +0700

    update

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,7 +1,19 @@
-import gspread
+import gspread, sys, getopt
 from oauth2client.service_account import ServiceAccountCredentials
- 
- 
+
+
+input_file = ''
+print(input_file + "awal")
+try:
+        opts, args = getopt.getopt(argv, "i", ["ifeature="])
+except getopt.GetoptError:
+        print("spreadsheet.py -f <feature name>")
+        sys.exit(2)
+for opt, arg in opts:
+        if opt in ("-f", "--ifeature"):
+            input_file = arg
+
+print(input_file + "tengah")
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
@@ -9,7 +21,8 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', s
 client = gspread.authorize(creds) 
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
-sheet = client.open("CHAT").sheet1
+sheet = client.open(input_file).sheet1
+print(input_file)
  
 # Extract and print all of the values
 list_of_hashes = sheet.get_all_records()
@@ -18,14 +31,14 @@ list_of_hashes = sheet.get_all_records()
 
 test_scenario = 'Feature: '+sheet.title+'\n\n'
 for testcase in list_of_hashes:
-	if(testcase['Test ID']!=''):
-		test_scenario += "\tScenario: " + testcase['Scenario']+'\n'
-	test_scenario += '\t\t' + testcase['Steps']+'\n'
+    if(testcase['Test ID']!=''):
+        test_scenario += "\tScenario: " + testcase['Scenario']+'\n'
+    test_scenario += '\t\t' + testcase['Steps']+'\n'
 
 # print(test_scenario)
 
-f= open(sheet.title + ".features","w+")
+f= open("/Users/ramurez/bukalapak_optimus/src/test/resources/features/"+ sheet.title + ".features","w+")
 f.write(test_scenario)
-f.close()
 
+f.close()
 
